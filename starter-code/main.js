31d6d13a4a38c16c7d8cea38c444c6f42111646e
Chon Lewis <chonlewis@Chon-Lewiss-iMac.local>
2016-09-15 17:06:14 -0500

    unit 10

--- starter-code/main.js
+++ starter-code/main.js
@@ -5,15 +5,28 @@ var cardTwo = 'queen';
 var cardThree = 'king';
 var cardFour = 'king';
 
-if (cardOne === cardTwo) {
-	alert('You found a match!')
-}
-else if (cardOne !== cardTwo) {
-	alert('Sorry, try again.')
-}
-else if (cardThree === cardFour) {
-	alert('You found a match!')
-}
-else if (cardThree !== cardFour) {
-	alert('Sorry, try again!')
+//if (cardOne === cardTwo) {
+//	alert('You found a match!')
+//}
+//else if (cardOne !== cardTwo) {
+//	alert('Sorry, try again.')
+//}
+//else if (cardThree === cardFour) {
+//	alert('You found a match!')
+//}
+//else if (cardThree !== cardFour) {
+//	alert('Sorry, try again!')
+//}
+
+
+var gameBoard = document.getElementById('game-board');
+
+function createBoard() {
+	for (var i = 0; i < x; i = i+1) {
+
+	var cardElement = document.createElement('div');
+
+	cardElement.className = 'card';
+	board.appendChild(cardElement);
+	}
 }
